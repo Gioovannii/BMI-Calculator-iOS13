b02344664b054fb6d54b2411b1bd040ef5b4de8c
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-03-26 15:50:06 +0800

    Create struct

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by mac on 2020/3/26.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+}
